de943c8acc2316781c774dc010f6a35b9bc96780
Aeness <dev@aeness.net>
2020-10-27 18:30:29 +0100

    Remove old commentaries

--- scrum-planif-FE/src/app/_rooms/io-websocket.service.ts
+++ scrum-planif-FE/src/app/_rooms/io-websocket.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import * as   io from 'socket.io-client';
+import * as io from 'socket.io-client';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import Debug from "debug";
@@ -8,7 +8,7 @@ const debug = Debug("scrum-planif:clientIo");
 @Injectable()
 export class IoWebsocketService implements OnDestroy {
 
-  protected socket; // Socket
+  protected socket; // Socket -  SocketIOClient.Socket
   protected nameRoom: String
 
   constructor() { }
@@ -68,37 +68,17 @@ export class IoWebsocketService implements OnDestroy {
   }
 
   ngOnDestroy() {
-    // We can because each IowebsocketService have is own Soket :
-    // All IowebsocketService use the same namespace
+    // We can because each IoWebsocketService have is own Soket :
+    // All IoWebsocketService use the same namespace
     // and with io.socket reusing the same namespace will also create two connections/Manager/Socket
     // https://socket.io/docs/client-api/#With-multiplexing
     // This call also destroy the Socket and close and destroy the Manager
 
     // The socket can be undefinited if we destroy the component before call
-    // init (append when we display a new event)
+    // init
 
-    // TODO éviter
-    /*
-    [WDS] Disconnected! client:176
-close
-client:176
-onclose
-socket.js:17
-[5]</EventTarget.prototype.dispatchEvent
-sockjs.js:170
-[14]</</SockJS.prototype._close/<
-sockjs.js:965
-ZoneDelegate.prototype.invokeTask
-zone.js:421
-Zone.prototype.runTask
-zone.js:188
-ZoneTask.invokeTask
-zone.js:496
-ZoneTask/this.invoke
-zone.js:485
-timer
-zone.js:2054
-*/
-    if (this.socket) this.socket.disconnect();
+    if (this.socket) {
+      this.socket.disconnect();
+    }
   }
 }
