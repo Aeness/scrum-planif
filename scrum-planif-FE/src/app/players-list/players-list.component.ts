ffd8be76a16fc940a626d9b4e794afaf727b54fc
Aeness <dev@aeness.net>
2020-11-28 17:26:25 +0100

    List the votes fot the administrator (scrum master).

--- scrum-planif-FE/src/app/players-list/players-list.component.ts
+++ scrum-planif-FE/src/app/players-list/players-list.component.ts
@@ -1,46 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { PlanifRoom } from '../planif.room/planif.room';
-import { Player } from '../auth.service/player';
-import { Observable, of } from 'rxjs';
+import { Component } from '@angular/core';
+import { PlayersComponent } from '../players/players.component';
 
 @Component({
   selector: 'app-players-list',
-  templateUrl: 'players-list.component.html',
-  providers:  [ PlanifRoom ] // TODO to keep ?
+  templateUrl: 'players-list.component.html'
 })
-export class PlayersListComponent implements OnInit {
-
-  @Input() planifRoom: PlanifRoom;
-
-  players$: Observable<Map<String, Player>> ;
-  protected players: Map<String, Player> = new Map<String, Player>();
-
-  constructor() { }
-
-  ngOnInit() {
-
-    this.planifRoom.askPlayersList().then(
-      (data: Map<String, Player>) => {
-        data.forEach((player, ref_player) => {
-          this.players.set(ref_player, player);
-        })
-
-        this.players$ = of(this.players);
-
-        this.planifRoom.listenPlayerJoinPlanif().subscribe(
-          (data: { player: Player; }) => {
-            this.players.set(data.player.ref, data.player);
-          }
-        );
-
-        this.planifRoom.listenPlayerQuitPlanif().subscribe(
-          (data: { player_ref: String; }) => {
-            console.log("deletePlayer");
-            console.log(data.player_ref);
-            this.players.delete(data.player_ref);
-          }
-        );
-      }
-    );
-  }
-}
+export class PlayersListComponent extends PlayersComponent { }
