3d076245b857b4bd2f624940dd66f11d6658289f
Aeness <124333+Aeness@users.noreply.github.com>
2021-03-13 11:36:26 +0100

    Angular 10.2.4 => 11.2.5

--- scrum-planif-FE/src/app/app.module.ts
+++ scrum-planif-FE/src/app/app.module.ts
@@ -70,7 +70,7 @@ const appRoutes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: environment.routerEnableTracing } // <-- debugging purposes only
+      { enableTracing: environment.routerEnableTracing, relativeLinkResolution: 'legacy' } // <-- debugging purposes only
     ),
     ReactiveFormsModule,
     FontAwesomeModule
